d30c028d03f3d13d3a1dce0b145ecc7799efff55
IanWachcode <ianwachira90@gmail.com>
2025-12-06 21:53:06 +0300

    final commit

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -1,10 +1,18 @@
 const hamburger = document.getElementById( 'hamburger' );
-const navLinks = document.getElementById( 'nav-lists' );
+const navLinks = document.getElementById( 'navLinks' );
 
 hamburger.addEventListener( 'click', () =>
     {
     navLinks.classList.toggle( 'active' );
 
         // Optional: animate hamburger to X
-    hamburger.classList.toggle( 'open' );
+    hamburger.classList.toggle( 'active' );
+    navLinks.classList.toggle( 'active' );
 } );
+//
+document.querySelectorAll("#navLinks a").forEach(link =>
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navLinks.classList.remove("active");
+    })
+);
